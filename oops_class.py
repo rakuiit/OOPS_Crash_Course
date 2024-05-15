12d46e49343b4e489af1be0d0d5daba52b79f0c4
rakeshsharmaMB <rakesh.sharma@gmail.com>
2024-05-15 08:00:58 +0530

    first commit

---
+++ oops_class.py
@@ -0,0 +1,12 @@
+
+# position,name, age,level,salary
+se1=["Software Engineer","Max",20,"Junior",5000]
+se2=["Software Engineer","Lisa",25,"Junior",7000]
+
+# class
+class SoftwareEngineer:
+    pass
+
+
+#instance
+se1=SoftwareEngineer()
